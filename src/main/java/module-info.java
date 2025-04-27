876f380480431095f16bdf3815b9acae5e0a0ebe
Michael Dudzik <mkdudik15@gmail.com>
2025-04-27 14:35:10 +0300

    adding some packects to open some root for export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,7 @@ module com.example.server {
     exports com.example.server.model;
     opens com.example.server.model to com.fasterxml.jackson.databind;
     exports com.example.server.dao to com.fasterxml.jackson.databind;
+    //добавил пакеты
+    opens com.example.server to com.fasterxml.jackson.databind;
+    opens com.example.server.service to com.fasterxml.jackson.databind;
 }
